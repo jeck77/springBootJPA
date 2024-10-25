05085793810d9ae97bd29f3ff728953012a8a86a
ajm <jeck77@naver.com>
2024-10-25 21:45:56 +0900

    상품 도메인 추가

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name="dtype")
-@Getter @Setter
+@Getter
 public abstract class Item {
     @Id
     @GeneratedValue
@@ -24,4 +24,24 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // ==== 비즈니스 로직 ==== //
+
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0) {
+            throw new jpabook.jpashop.exception.NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
