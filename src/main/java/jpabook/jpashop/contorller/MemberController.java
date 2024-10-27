d419760513a5799655252b08368801c7d43d8df0
ajm <jeck77@naver.com>
2024-10-27 21:46:22 +0900

    회원 리스트 추가

--- src/main/java/jpabook/jpashop/contorller/MemberController.java
+++ src/main/java/jpabook/jpashop/contorller/MemberController.java
@@ -5,14 +5,18 @@ import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class MemberController {
 
     private final MemberService memberService;
@@ -20,14 +24,14 @@ public class MemberController {
     @GetMapping("/members/new")
     public String createForm(Model model){
         model.addAttribute("memberForm", new MemberForm());
-        return "members/createMemberForm.html";
+        return "members/createMemberForm";
     }
 
     @PostMapping("/members/new")
     public String create(@Valid MemberForm form, BindingResult result){
         // BindingResult 에러처리
         if(result.hasErrors()){
-            return "members/createMemberForm.html";
+            return "members/createMemberForm";
         }
 
         Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
@@ -36,5 +40,15 @@ public class MemberController {
         member.setName(form.getName());
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        // 보통은 외부에 엔티티 자체를 넘기지 않고 특히 API
+        // DTO를 통해서 필요한 정보만 넘김
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+
+        return "members/memberList";
     }
 }
