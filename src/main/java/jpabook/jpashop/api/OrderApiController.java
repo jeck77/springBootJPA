2987372421aea5a59259c2abd930dde4b0d0b2fc
ajm <jeck77@naver.com>
2024-12-03 00:56:34 +0900

    조회성능 최적화

---
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,86 @@
+package jpabook.jpashop.api;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    /**
+     * 권장하는 방식 x
+     */
+    @GetMapping("/api/v1/orders")
+    public List<Order> orderV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+            List<OrderItem> orderItems = order.getOrderItems();
+
+            orderItems.stream().forEach(o -> o.getItem().getName());
+        }
+
+        return all;
+    }
+
+    /**
+     * Dto 안에 엔티티가 있을 경우 Dto를 하나 더 생성하여 받아준다. 단 쿼리 실행이 많기 때문에 권장 x
+     */
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> orderV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<OrderDto> collect = orders.stream().map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+
+        return collect;
+    }
+
+    @Getter
+    static class OrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<orderItemDto> orderItems;
+
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+            orderItems = order.getOrderItems().stream()
+                    .map(orderItem -> new orderItemDto(orderItem))
+                    .collect(Collectors.toList());
+        }
+    }
+
+    @Getter
+    static class orderItemDto {
+        private String itemName;
+        private int orderPrice;
+        private int count;
+
+        public orderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
+        }
+    }
+}
